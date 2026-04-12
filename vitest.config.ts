9b50ae1ea126f582548436658016290c87468f2c
art0rz <art0rz@protonmail.com>
2026-04-12 20:10:38 +0200

    chore: upgrade vitest and add coverage support

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ const config = defineConfig({
   test: {
     globals: true,
     exclude: ['**/cjs/**', 'node_modules', 'dist'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
   },
 });
 
